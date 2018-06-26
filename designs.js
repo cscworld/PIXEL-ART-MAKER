07d9cbfb61469bc0d17a2c73819f8076ce922286
Ekiotenne Ebiyei James <40577169+cscworld@users.noreply.github.com>
2018-06-26 21:11:36 +0100

    ALC Pixel Art Maker Project

---
+++ designs.js
@@ -0,0 +1,40 @@
+function makeGrid(){
+    var height, width,color,  canvas, canvasCell;
+
+    height = $('#inputHeight').val();
+    width = $('#inputWeight').val();
+    canvas= $('#pixelCanvas');
+
+    // to clear the table
+    canvas.children().remove();
+
+    //To dynamically add table row base on width
+    for (var x =1; x <= height; x++) {
+    canvas.append('<tr></tr>');
+    }
+        canvasCell=$('tr');
+
+    //To dynamically add table colum base on widths
+    for(var y = 1; y <= width; y++){
+        canvasCell.append('<td></td>');
+    }
+      assign=canvas.find('td');
+
+    //to set the  the color picked
+    assign.click(function(){
+        color = $('#colorPicker').val();
+        $(this).css('background-color',color);
+    });
+
+}
+var submit;
+
+$(document).ready(function(){
+    submit = $('input[type="submit"]');
+    
+    submit.click(function(event) {
+         event.preventDefault();
+         makeGrid();
+    });
+
+});
